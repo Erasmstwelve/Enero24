bb1adec8055aa28fcafabdfad9f79ab3e4193cca
Erasmstwelve <mvictoria1232@gmail.com>
2024-01-20 15:51:35 -0500

    Ejercicio3 resuelto

--- ejercicio 3/buscadorPalabras.py
+++ ejercicio 3/buscadorPalabras.py
@@ -1,7 +1,10 @@
-def contar_palabra(texto, palabra)
+#se definen variables
+def contar_palabra(texto, palabra): #faltaba el signo :
+    #return devuelve un valor
     return texto.lower().split().count(palabra.lower())
 
-texto = "Este es un ejemplo de texto. Este texto tiene palabras repetidas."
+texto = "Este es un ejemplo de texto- Este texto tiene palabras repetidas."
 palabra_buscada = "texto"
 
-print(f"La palabra '{palabra_buscada aparece contar_palabra(texto, palabra_buscada)} veces.")
+#imprime la palabra repetida
+print(f"La palabra '{palabra_buscada}' aparece {contar_palabra(texto, palabra_buscada)} veces.") #añadí signos {} que faltaban a la syntaxis del texto
