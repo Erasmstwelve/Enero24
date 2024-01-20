dbf8e042947c26292268aa604df6dad7bdd01a20
Erasmstwelve <mvictoria1232@gmail.com>
2024-01-20 15:41:47 -0500

    Ejercicio2 resuelto

--- ejercicio 1/calculadora.py
+++ ejercicio 1/calculadora.py
@@ -1,8 +1,12 @@
+#def define las variables a usar en el codigo
 def calculadora():
+    #Aqui se estan declarando las variables num1 y num 2 en float, al igual que se usa el input para recoger una entrada de texto
     num1 = float(input("Ingrese el primer número: ")) #Error = no se habia puesto el valor input
     num2 = float(input("Ingrese el segundo número: "))
+    #Aqui se define la variable operacion que se usa para elegir que tipo de operación utilizaremos
     operacion = input("Ingrese la operación (+, -, *, /): ")
 
+    #Condicionales para funcionamiento de la entrada de datos y su resultado
     if operacion == '+':
         resultado = num1 + num2 #Error= no se declaró la variable num, correccion Num1
     elif operacion == '-':
@@ -14,6 +18,8 @@ def calculadora():
     else:
         resultado = "Operación no válida"
 
+    #Imprime el resultado de la operacion dada en la entrada de datos del usuario
     print("Resultado:" , resultado) #Error = faltaba una coma despues de la variable "Resultado:"
 
+#cierra la variable calculadora
 calculadora() #Error= la variable estaba mal escrita = anterior: "Calculdora" 
--- ejercicio 2/contraseñas.py
+++ ejercicio 2/contraseñas.py
@@ -1,9 +1,14 @@
-import rando
+#import me funciona para elegir librerias
+import random #Corregí la libreria rando por random, ya que estaba mal escrita
 import string
 
-generar_contraseña(longitud=8):
+#def me ayuda a definir la variable
+#longitud=8 me ayuda a definir que la variable debe tener 8 caracteres
+def generar_contraseña(longitud=8): #no estaba definida la variablle generar_contraseña
+
+    #dos variables en string 
     caracteres = string.ascii_letters + string.digits + string.punctuation
     contraseña = ''.join(random.choice(caracteres) for _ in range(longitud))
-    retunr contraseña
+    return contraseña #corregi "retunr" por return, mal escrita.
 
-print(generar_contrasena())
+print(generar_contraseña())
