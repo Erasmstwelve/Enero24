8b2aa2c9aef098512e8664e998c9af5a725a1413
Erasmstwelve <mvictoria1232@gmail.com>
2024-01-27 16:25:58 -0500

    Ejercicio8 resuelto

--- ejercicio 8/contadorPalabras.py
+++ ejercicio 8/contadorPalabras.py
@@ -1,11 +1,12 @@
-def contar_palabras_en_archivo(nombre_archivo)
+def contar_palabras_en_archivo(nombre_archivo):  # Faltaba el signo :
     try:
         with open(nombre_archivo, 'r') as archivo:
             contenido = archivo.read()
             palabras = contenido.split()
             return len(palabras)
     except FileNotFoundError:
-        retunr f"El archivo {nombre_archivo} no fue encontrado."
+        return f"El archivo {nombre_archivo} no fue encontrado."  # Estaba mal escrita la palabra (return)
 
 archivo_nombre = input("Ingrese el nombre del archivo de texto: ")
 print(f"El archivo contiene {contar_palabras_en_archivo(archivo_nombre)} palabras.")
+
