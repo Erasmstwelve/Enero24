ae89446bd6bc004b174611d85d3b611c26d8ea3b
Erasmstwelve <mvictoria1232@gmail.com>
2024-01-27 16:36:15 -0500

    Ejercicio10 resuelto

--- ejercicio 10/descuento.py
+++ ejercicio 10/descuento.py
@@ -1,9 +1,10 @@
-calcular_precio_con_decuento(precio_base, porcentaje_descuento)
+def calcular_precio_con_descuento(precio_base, porcentaje_descuento): #Faltaba declarar el def y agregarle los :
     descuento = precio_base * (porcentaje_descuento / 100)
     precio_final = precio_base - descuento
-    retunr precio_final
+    return precio_final #Estaba mal escrita
 
-precio_base = float("Ingrese el precio base del producto: ")
+precio_base = float(input("Ingrese el precio base del producto: ")) #Faltaba el input
 porcentaje_descuento = float(input("Ingrese el porcentaje de descuento: "))
 precio_final = calcular_precio_con_descuento(precio_base, porcentaje_descuento)
 print(f"El precio final con {porcentaje_descuento}% de descuento es: {precio_final}")
+
