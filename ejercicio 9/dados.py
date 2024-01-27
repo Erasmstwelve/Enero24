ca42d143e103c013b8d3e8ae2c65fa4d091d6b29
Erasmstwelve <mvictoria1232@gmail.com>
2024-01-27 16:32:27 -0500

    Ejercicio9 resuelto

--- ejercicio 9/dados.py
+++ ejercicio 9/dados.py
@@ -1,10 +1,11 @@
-import radom
+import random #Estaba mal escrito
 
-simular_lanzamiento_dados(cantidad_dados, caras_por_dado)
+def simular_lanzamiento_dados(cantidad_dados, caras_por_dado): #No tenía el def y le faltaban los :
     resultados = [random.randint(1, caras_por_dado) for _ in range(cantidad_dados)]
-    retunr resultados
+    return resultados #La palabra retunr estaba mal escrita
 
 cantidad_dados = int(input("Ingrese la cantidad de dados a lanzar: "))
 caras_por_dado = int(input("Ingrese la cantidad de caras por dado: "))
-lanzamientos = simular_lanzamiento_dados(, caras_por_dado)
+lanzamientos = simular_lanzamiento_dados(cantidad_dados, caras_por_dado) #Le faltaba definir otro argumento
 print(f"Resultados del lanzamiento: {lanzamientos}")
+
